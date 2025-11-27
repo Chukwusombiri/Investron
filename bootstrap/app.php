431bc6c6039691e5e5657056be6e7fc6ec5b3516
Chisom Okwuosa <okwuosachisom8@gmail.com>
2025-11-27 13:20:48 +0100

    vvvvvv

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,9 +25,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         ]);
 
-        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST |
+        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO);
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB
+    );
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
